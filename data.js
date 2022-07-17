ecc54ee06787f4e253cec22ff39235ed9e22d43e
Merouane <merouane.souadji@efrei.net>
2022-07-18 00:24:11 +0200

    Css et quelques corrections

--- data.js
+++ data.js
@@ -1,11 +1,12 @@
 const ctf_list = [
     {
       image: './images/thm_ctf_1.png',
-      description: 'Description du ctf 1',
+      description: "In these set of tasks you'll learn the following  : brute forcing, hash cracking, service enumeration, Linux Enumeration, The main goal here is to learn as much as possible. Make sure you are connected to our network using your OpenVPN configuration file. Credits to Josiah Pierce from Vulnhub.",
       titre: 'Basic Pentesting',
-      lien:'lien vers le ctf',
-      difficulte: 'difficulte du ctf',
-      temps:"temps du ctf"
+      avis: '3 etoiles',
+      lien:'https://tryhackme.com/room/basicpentestingjt',
+      difficulte: 'easy',
+      temps:"1 heure"
     },
     {
       image: './images/thm_ctf_2.png',
@@ -20,6 +21,7 @@ const ctf_list = [
       image: './images/thm_ctf_3.png',
       description: 'Description du ctf 2',
       titre: 'Titre du ctf',
+      avis: 'avis du ctf',
       lien:'lien vers le ctf',
       difficulte: 'difficulte du ctf',
       temps:"temps du ctf"
@@ -29,6 +31,7 @@ const ctf_list = [
       image: './images/thm_ctf_4.png',
       description: 'Description du ctf 2',
       titre: 'Titre du ctf',
+      avis: 'avis du ctf',
       lien:'lien vers le ctf',
       difficulte: 'difficulte du ctf',
       temps:"temps du ctf"
@@ -38,6 +41,7 @@ const ctf_list = [
       image: './images/thm_ctf_5.png',
       description: 'Description du ctf 2',
       titre: 'Titre du ctf',
+      avis: 'avis du ctf',
       lien:'lien vers le ctf',
       difficulte: 'difficulte du ctf',
       temps:"temps du ctf"
@@ -47,6 +51,7 @@ const ctf_list = [
       image: './images/thm_ctf_6.png',
       description: 'Description du ctf 2',
       titre: 'Titre du ctf',
+      avis: 'avis du ctf',
       lien:'lien vers le ctf',
       difficulte: 'difficulte du ctf',
       temps:"temps du ctf"
@@ -56,6 +61,7 @@ const ctf_list = [
       image: './images/thm_ctf_0.png',
       description: 'Description du ctf 2',
       titre: 'Titre du ctf',
+      avis: 'avis du ctf',
       lien:'lien vers le ctf',
       difficulte: 'difficulte du ctf',
       temps:"temps du ctf"
@@ -65,6 +71,7 @@ const ctf_list = [
       image: './images/thm_ctf_0.png',
       description: 'Description du ctf 2',
       titre: 'Titre du ctf',
+      avis: 'avis du ctf',
       lien:'lien vers le ctf',
       difficulte: 'difficulte du ctf',
       temps:"temps du ctf"
@@ -74,6 +81,7 @@ const ctf_list = [
       image: './images/thm_ctf_0.png',
       description: 'Description du ctf 2',
       titre: 'Titre du ctf',
+      avis: 'avis du ctf',
       lien:'lien vers le ctf',
       difficulte: 'difficulte du ctf',
       temps:"temps du ctf"
