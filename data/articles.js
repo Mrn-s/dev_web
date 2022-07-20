802241812bf430587587fd6d2f5c0d039b2bcee6
Merouane <merouane.souadji@efrei.net>
2022-07-20 23:07:11 +0200

    Maj

--- data/articles.js
+++ data/articles.js
@@ -1,11 +1,11 @@
-const article_list = [
+const articles_list = [
     {
       image: './images/thm_ctf_1.png',
       description: "In these set of tasks you'll learn the following  : brute forcing, hash cracking, service enumeration, Linux Enumeration, The main goal here is to learn as much as possible. Make sure you are connected to our network using your OpenVPN configuration file. Credits to Josiah Pierce from Vulnhub.",
-      titre: 'Basic Pentesting',
-      lien:'https://tryhackme.com/room/basicpentestingjt',
-      theme: 'easy',
-      temps:"1 heure"
+      titre: 'Poutine, l\'incontrôlable',
+      lien:'https://www.youtube.com/watch?v=gGQ9tD0bA1U',
+      theme: 'L\'etranger',
+      temps:"26 mins"
     },
     {
       image: './images/thm_ctf_2.png',
